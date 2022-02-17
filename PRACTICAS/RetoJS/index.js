68ff7bcc549e380da5c377415123ce8d0582e661
ivansmb11 <ismb9@hotmail.com>
2022-02-17 00:41:20 -0600

    retojs

--- PRACTICAS/RetoJS/index.js
+++ PRACTICAS/RetoJS/index.js
@@ -3,6 +3,7 @@ const ids = [];
 for (let index = 1438; index <= 1834; index++) {
   ids[index] = index;
   document.write(`${ index } \n`);
+  // console.log(index,'\n');
 }
 
 // console.log(ids.length); para NODE.js
