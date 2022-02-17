b1603e12da9f88a2036d0045b6e3b0745f1f99a0
ivansmb11 <ismb9@hotmail.com>
2022-02-17 11:24:47 -0600

    curso

---
+++ js essentials/myapp.js
@@ -0,0 +1,13 @@
+console.log( "Hello world" );
+
+console.log( 200 );
+
+console.log( true );
+
+console.log( null );
+
+var num = 10;
+
+console.log( num );
+
+console.log( "string", 10.11, true, false, null, num );
