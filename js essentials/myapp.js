6f2ca0279776972431ee3886979fd5b2f46eaf4e
ivansmb11 <ismb9@hotmail.com>
2022-02-20 22:58:52 -0600

    1

--- js essentials/myapp.js
+++ js essentials/myapp.js
@@ -6,8 +6,19 @@ console.log( true );
 
 console.log( null );
 
-var num = 10;
+let num = 10;
 
 console.log( num );
 
 console.log( "string", 10.11, true, false, null, num );
+
+const myFunction = n1 => {
+
+    const n2 = 2;
+
+    const result = n1+n2;
+    console.log( result );
+
+}
+
+myFunction(3);
