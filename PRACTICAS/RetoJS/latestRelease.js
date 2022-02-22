8f5c8804ba08fe3cb9dd4d08b27d94b916177fdc
ivai <ismb9@hotmail.com>
2022-02-21 20:17:00 -0600

    creacion de elementos html con js

--- PRACTICAS/RetoJS/latestRelease.js
+++ PRACTICAS/RetoJS/latestRelease.js
@@ -20,7 +20,7 @@ const findByID = query => {
 
     // document.getElementById('Nombre').value='';
 
-    var idQ = parseInt(document.getElementById('queryID').value);
+    let idQ = parseInt(document.getElementById('queryID').value);
     
     const user = users[idQ];
 
@@ -28,8 +28,39 @@ const findByID = query => {
       if (user.id === idQ) {
         document.getElementById('nombreT').value=user.name;
         document.getElementById('generoT').value=user.gender;
+
+        const { id, name, gender } = user;
+        
+        // const id = user.id;
+        // const name = user.name;
+        // const gender = user.gender;
+
+        let divInfo = document.getElementById('busquedas');
+
+        let cajaInfo = document.createElement('div');
+        let listaH = document.createElement('ul');
+        let liID = document.createElement('li');
+        let liName = document.createElement('li');
+        let liGender = document.createElement('li');
+
+        const textID = document.createTextNode(`Id: ${ id }`);
+        const textName = document.createTextNode(`Name: ${ name }`);
+        const textGender = document.createTextNode(`Gender: ${ gender }`);
+
+        liID.appendChild(textID);
+        liName.appendChild(textName);
+        liGender.appendChild(textGender);
+
+        cajaInfo.className = 'cajaQuery';
+        divInfo.appendChild(cajaInfo);
+        cajaInfo.appendChild(listaH);
+        listaH.appendChild(liID);
+        listaH.appendChild(liName);
+        listaH.appendChild(liGender);
+
       }
-    } catch {
+    } catch(error) {
+      console.log(error);
       document.getElementById('nombreT').value='';
       document.getElementById('generoT').value='';
       window.alert('ERROR 404:\nNo se encontró a nadie con ese ID.')
