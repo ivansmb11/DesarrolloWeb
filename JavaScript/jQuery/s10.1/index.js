322287ee7d53bc7a5ddf2d09cb3992aa5e91ac1b
ivai <ismb9@hotmail.com>
2022-03-15 13:03:18 -0600

    s10.1

---
+++ JavaScript/jQuery/s10.1/index.js
@@ -0,0 +1,20 @@
+
+// $(document).ready(function(){
+//     alert('Hola jQuery');
+// });
+
+// $(function(){
+
+// });
+
+$(document).ready(function(){
+    $('h1').hide();
+    $('h1, h2').hide();
+    $('.encabezado').hide();
+    $('#boton').hide();
+
+    $('p.primero').hide();
+    $('p#primero').hide();
+
+    $('.primero a').hide();
+});
