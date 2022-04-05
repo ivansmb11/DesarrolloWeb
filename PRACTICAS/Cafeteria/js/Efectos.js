c3c4037a0d3eb79cb03f44e6cef86870b76753b2
ivai <ismb9@hotmail.com>
2022-04-04 19:31:24 -0500

    generated efectos.js

---
+++ PRACTICAS/Cafeteria/js/Efectos.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    //Efecto para el menu horizontal
+    $('.menu a').each(function(index, elemento){
+        $(this).css({
+            'top': '-200px'
+        });
+
+        $(this).animate({
+            top: '0'
+        }, 2000 + (index * 500));
+    });
+
+});
