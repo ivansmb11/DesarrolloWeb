44898cacb483622c87bdca7c337c813ff506ec9f
ivai <ismb9@hotmail.com>
2022-04-04 19:43:13 -0500

    generated efectos.js2

--- PRACTICAS/Cafeteria/js/Efectos.js
+++ PRACTICAS/Cafeteria/js/Efectos.js
@@ -11,4 +11,17 @@ $(document).ready(function(){
         }, 2000 + (index * 500));
     });
 
+    //Efecto Header
+    if ($(window).width() > 800) {
+        $('header .textos').css({
+            opacity: 0,
+            marginTop: 0
+        });
+
+        $('header .textos').animate({
+            opacity: 1,
+            marginTop: '-52px'
+        }, 1500);
+    }
+
 });
