2a1f55287664881f1553b5a69557b310fbf89d01
ivai <ismb9@hotmail.com>
2022-04-04 19:49:01 -0500

    scroll Efectos.js

--- PRACTICAS/Cafeteria/js/Efectos.js
+++ PRACTICAS/Cafeteria/js/Efectos.js
@@ -24,4 +24,42 @@ $(document).ready(function(){
         }, 1500);
     }
 
+    // Scroll Elemento menu
+    let acercaDe = $('#acerca-de').offset().top,
+    menu = $('#platillos').offset().top,
+    galeria = $('#galeria').offset().top,
+    ubicacion = $('#ubicacion').offset().top;
+    
+    //Boton "Acerca de"
+    $('#btn-acerca-de').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 500
+        }, 500);
+    });
+
+    //Boton "Menu
+    $('#btn-menu').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: menu - 40
+        });
+    });
+
+    //Boton "Galeria"
+    $('#btn-galeria').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: galeria - 40
+        });
+    });
+
+    //Boton "Ubicacion"
+    $('#btn-ubicacion').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: ubicacion - 40
+        });
+    });
+
 });
